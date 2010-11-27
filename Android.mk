61cb230180231bbcff0166ebc90ca8e16aba05dc
Logan <tzuhsiang.chien@gmail.com>
2010-11-27 14:18:26 +0800

    Cleanup Android.mk.

--- Android.mk
+++ Android.mk
@@ -31,80 +31,80 @@ include $(CLEAR_VARS)
 LOCAL_PRELINK_MODULE := false
 LOCAL_MODULE := libbcc
 LOCAL_MODULE_TAGS := optional
-LOCAL_SRC_FILES :=	\
-	bcc.cpp	\
-	bcc_runtime.c	\
+LOCAL_SRC_FILES := \
+  bcc.cpp \
+  bcc_runtime.c \
   bcc_compiler.cpp \
   bcc_code_emitter.cpp \
   bcc_code_mem_manager.cpp \
-	runtime/lib/arm/adddf3vfp.S	\
-	runtime/lib/arm/addsf3vfp.S	\
-	runtime/lib/arm/divdf3vfp.S	\
-	runtime/lib/arm/divsf3vfp.S	\
-	runtime/lib/arm/eqdf2vfp.S	\
-	runtime/lib/arm/eqsf2vfp.S	\
-	runtime/lib/arm/extendsfdf2vfp.S	\
-	runtime/lib/arm/fixdfsivfp.S	\
-	runtime/lib/arm/fixsfsivfp.S	\
-	runtime/lib/arm/fixunsdfsivfp.S	\
-	runtime/lib/arm/fixunssfsivfp.S	\
-	runtime/lib/arm/floatsidfvfp.S	\
-	runtime/lib/arm/floatsisfvfp.S	\
-	runtime/lib/arm/floatunssidfvfp.S	\
-	runtime/lib/arm/floatunssisfvfp.S	\
-	runtime/lib/arm/gedf2vfp.S	\
-	runtime/lib/arm/gesf2vfp.S	\
-	runtime/lib/arm/gtdf2vfp.S	\
-	runtime/lib/arm/gtsf2vfp.S	\
-	runtime/lib/arm/ledf2vfp.S	\
-	runtime/lib/arm/lesf2vfp.S	\
-	runtime/lib/arm/ltdf2vfp.S	\
-	runtime/lib/arm/ltsf2vfp.S	\
-	runtime/lib/arm/muldf3vfp.S	\
-	runtime/lib/arm/mulsf3vfp.S	\
-	runtime/lib/arm/nedf2vfp.S	\
-	runtime/lib/arm/negdf2vfp.S	\
-	runtime/lib/arm/negsf2vfp.S	\
-	runtime/lib/arm/nesf2vfp.S	\
-	runtime/lib/arm/subdf3vfp.S	\
-	runtime/lib/arm/subsf3vfp.S	\
-	runtime/lib/arm/truncdfsf2vfp.S	\
-	runtime/lib/arm/unorddf2vfp.S	\
-	runtime/lib/arm/unordsf2vfp.S
+  runtime/lib/arm/adddf3vfp.S \
+  runtime/lib/arm/addsf3vfp.S \
+  runtime/lib/arm/divdf3vfp.S \
+  runtime/lib/arm/divsf3vfp.S \
+  runtime/lib/arm/eqdf2vfp.S \
+  runtime/lib/arm/eqsf2vfp.S \
+  runtime/lib/arm/extendsfdf2vfp.S \
+  runtime/lib/arm/fixdfsivfp.S \
+  runtime/lib/arm/fixsfsivfp.S \
+  runtime/lib/arm/fixunsdfsivfp.S \
+  runtime/lib/arm/fixunssfsivfp.S \
+  runtime/lib/arm/floatsidfvfp.S \
+  runtime/lib/arm/floatsisfvfp.S \
+  runtime/lib/arm/floatunssidfvfp.S \
+  runtime/lib/arm/floatunssisfvfp.S \
+  runtime/lib/arm/gedf2vfp.S \
+  runtime/lib/arm/gesf2vfp.S \
+  runtime/lib/arm/gtdf2vfp.S \
+  runtime/lib/arm/gtsf2vfp.S \
+  runtime/lib/arm/ledf2vfp.S \
+  runtime/lib/arm/lesf2vfp.S \
+  runtime/lib/arm/ltdf2vfp.S \
+  runtime/lib/arm/ltsf2vfp.S \
+  runtime/lib/arm/muldf3vfp.S \
+  runtime/lib/arm/mulsf3vfp.S \
+  runtime/lib/arm/nedf2vfp.S \
+  runtime/lib/arm/negdf2vfp.S \
+  runtime/lib/arm/negsf2vfp.S \
+  runtime/lib/arm/nesf2vfp.S \
+  runtime/lib/arm/subdf3vfp.S \
+  runtime/lib/arm/subsf3vfp.S \
+  runtime/lib/arm/truncdfsf2vfp.S \
+  runtime/lib/arm/unorddf2vfp.S \
+  runtime/lib/arm/unordsf2vfp.S
 
-LOCAL_STATIC_LIBRARIES :=	\
-	libLLVMARMCodeGen	\
-	libLLVMARMInfo	\
-	libLLVMBitReader	\
-	libLLVMSelectionDAG	\
-	libLLVMAsmPrinter	\
-	libLLVMCodeGen	\
-	libLLVMLinker	\
-	libLLVMJIT	\
-	libLLVMTarget	\
-	libLLVMMC	\
-	libLLVMScalarOpts	\
-	libLLVMInstCombine	\
-	libLLVMipo	\
-	libLLVMipa	\
-	libLLVMTransformUtils	\
-	libLLVMCore	\
-	libLLVMSupport	\
-	libLLVMSystem	\
-	libLLVMAnalysis
+LOCAL_STATIC_LIBRARIES := \
+  libLLVMARMCodeGen \
+  libLLVMARMInfo \
+  libLLVMBitReader \
+  libLLVMSelectionDAG \
+  libLLVMAsmPrinter \
+  libLLVMCodeGen \
+  libLLVMLinker \
+  libLLVMJIT \
+  libLLVMTarget \
+  libLLVMMC \
+  libLLVMScalarOpts \
+  libLLVMInstCombine \
+  libLLVMipo \
+  libLLVMipa \
+  libLLVMTransformUtils \
+  libLLVMCore \
+  libLLVMSupport \
+  libLLVMSystem \
+  libLLVMAnalysis
 
 LOCAL_SHARED_LIBRARIES := libdl libcutils libutils libstlport
 
-LOCAL_C_INCLUDES :=	\
-	$(LOCAL_PATH)/include
+LOCAL_C_INCLUDES := \
+  $(LOCAL_PATH)/include
 
 ifeq ($(USE_DISASSEMBLER),true)
 LOCAL_CFLAGS += -DUSE_DISASSEMBLER
-LOCAL_STATIC_LIBRARIES :=	\
-	libLLVMARMDisassembler	\
-	libLLVMARMAsmPrinter	\
-	libLLVMMCParser	\
-	$(LOCAL_STATIC_LIBRARIES)
+LOCAL_STATIC_LIBRARIES := \
+  libLLVMARMDisassembler \
+  libLLVMARMAsmPrinter \
+  libLLVMMCParser \
+  $(LOCAL_STATIC_LIBRARIES)
 endif
 
 # This hides most of the symbols in the shared library and reduces the size
@@ -120,54 +120,54 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE := libbcc
 LOCAL_MODULE_TAGS := optional
-LOCAL_SRC_FILES :=   \
-	bcc.cpp	     \
+LOCAL_SRC_FILES := \
+  bcc.cpp \
   bcc_compiler.cpp \
   bcc_code_emitter.cpp \
   bcc_code_mem_manager.cpp \
-	logd_write.c \
-	bcc_runtime.c
+  logd_write.c \
+  bcc_runtime.c
 
-LOCAL_STATIC_LIBRARIES :=	\
-	libcutils	\
-	libLLVMX86CodeGen	\
-	libLLVMX86Info	\
-	libLLVMARMCodeGen	\
-	libLLVMARMInfo	\
-	libLLVMBitReader	\
-	libLLVMSelectionDAG	\
-	libLLVMAsmPrinter	\
-	libLLVMMCParser	\
-	libLLVMCodeGen	\
-	libLLVMLinker	\
-	libLLVMJIT	\
-	libLLVMTarget	\
-	libLLVMMC	\
-	libLLVMScalarOpts	\
-	libLLVMInstCombine	\
-	libLLVMipo	\
-	libLLVMipa	\
-	libLLVMTransformUtils	\
-	libLLVMCore	\
-	libLLVMSupport	\
-	libLLVMSystem	\
-	libLLVMAnalysis
+LOCAL_STATIC_LIBRARIES := \
+  libcutils \
+  libLLVMX86CodeGen \
+  libLLVMX86Info \
+  libLLVMARMCodeGen \
+  libLLVMARMInfo \
+  libLLVMBitReader \
+  libLLVMSelectionDAG \
+  libLLVMAsmPrinter \
+  libLLVMMCParser \
+  libLLVMCodeGen \
+  libLLVMLinker \
+  libLLVMJIT \
+  libLLVMTarget \
+  libLLVMMC \
+  libLLVMScalarOpts \
+  libLLVMInstCombine \
+  libLLVMipo \
+  libLLVMipa \
+  libLLVMTransformUtils \
+  libLLVMCore \
+  libLLVMSupport \
+  libLLVMSystem \
+  libLLVMAnalysis
 
 LOCAL_LDLIBS := -ldl -lpthread
 
-LOCAL_C_INCLUDES :=	\
-	$(LOCAL_PATH)/include
+LOCAL_C_INCLUDES := \
+  $(LOCAL_PATH)/include
 
 # definitions for LLVM
 LOCAL_CFLAGS += -D__STDC_LIMIT_MACROS -D__STDC_CONSTANT_MACROS -DUSE_DISASSEMBLER=1 -DFORCE_ARM_CODEGEN=1 -DDEBUG_CODEGEN=1
 
 ifeq ($(USE_DISASSEMBLER),true)
 LOCAL_CFLAGS += -DUSE_DISASSEMBLER
-LOCAL_STATIC_LIBRARIES :=	\
-	libLLVMARMDisassembler	\
-	libLLVMARMAsmPrinter	\
-	libLLVMMCParser	\
-	$(LOCAL_STATIC_LIBRARIES)
+LOCAL_STATIC_LIBRARIES := \
+  libLLVMARMDisassembler \
+  libLLVMARMAsmPrinter \
+  libLLVMMCParser \
+  $(LOCAL_STATIC_LIBRARIES)
 endif
 
 include $(LLVM_ROOT_PATH)/llvm-host-build.mk
