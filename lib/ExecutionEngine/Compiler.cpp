6c0c7b03307a6e6dacb163df804a03b6bae36ce6
Shih-wei Liao <sliao@google.com>
2011-05-21 21:47:14 -0700

    Use FileHandle for MC object file
    Change-Id: If6330237145c9ca66c820db1495e9745a90f7aea

--- lib/ExecutionEngine/Compiler.cpp
+++ lib/ExecutionEngine/Compiler.cpp
@@ -20,6 +20,7 @@
 
 #include "ContextManager.h"
 #include "DebugHelper.h"
+#include "FileHandle.h"
 #include "ScriptCompiled.h"
 #include "Sha1Helper.h"
 
@@ -316,9 +317,9 @@ int Compiler::compile() {
   }
 
   int Fd = -1;
-  Fd = open(objPath.c_str(), O_CREAT | O_RDWR | O_TRUNC,
-            S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH /* 0644 */);
+  FileHandle file;
 
+  Fd = file.open(objPath.c_str(), OpenMode::Write);
   if (Fd < 0) {
     LOGE("Fail to open file '%s'", objPath.c_str());
     return 1;
