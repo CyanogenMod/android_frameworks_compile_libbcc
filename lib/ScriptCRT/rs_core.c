f9931b82453072d66b04b94bf30544df8f447eec
Jason Sams <rjsams@android.com>
2011-07-26 17:28:38 -0700

    Add const variants to matrix x vector multiply.
    Change-Id: Ied372bb02de49f8a18f006c03d1b859130bc3bdc

--- lib/ScriptCRT/rs_core.c
+++ lib/ScriptCRT/rs_core.c
@@ -101,7 +101,7 @@ rsMatrixGet(const rs_matrix2x2 *m, uint32_t row, uint32_t col) {
 
 
 extern float4 __attribute__((overloadable))
-rsMatrixMultiply(rs_matrix4x4 *m, float4 in) {
+rsMatrixMultiply(const rs_matrix4x4 *m, float4 in) {
     float4 ret;
     ret.x = (m->m[0] * in.x) + (m->m[4] * in.y) + (m->m[8] * in.z) + (m->m[12] * in.w);
     ret.y = (m->m[1] * in.x) + (m->m[5] * in.y) + (m->m[9] * in.z) + (m->m[13] * in.w);
@@ -109,9 +109,13 @@ rsMatrixMultiply(rs_matrix4x4 *m, float4 in) {
     ret.w = (m->m[3] * in.x) + (m->m[7] * in.y) + (m->m[11] * in.z) + (m->m[15] * in.w);
     return ret;
 }
+extern float4 __attribute__((overloadable))
+rsMatrixMultiply(rs_matrix4x4 *m, float4 in) {
+    return rsMatrixMultiply((const rs_matrix4x4 *)m, in);
+}
 
 extern float4 __attribute__((overloadable))
-rsMatrixMultiply(rs_matrix4x4 *m, float3 in) {
+rsMatrixMultiply(const rs_matrix4x4 *m, float3 in) {
     float4 ret;
     ret.x = (m->m[0] * in.x) + (m->m[4] * in.y) + (m->m[8] * in.z) + m->m[12];
     ret.y = (m->m[1] * in.x) + (m->m[5] * in.y) + (m->m[9] * in.z) + m->m[13];
@@ -119,9 +123,13 @@ rsMatrixMultiply(rs_matrix4x4 *m, float3 in) {
     ret.w = (m->m[3] * in.x) + (m->m[7] * in.y) + (m->m[11] * in.z) + m->m[15];
     return ret;
 }
+extern float4 __attribute__((overloadable))
+rsMatrixMultiply(rs_matrix4x4 *m, float3 in) {
+    return rsMatrixMultiply((const rs_matrix4x4 *)m, in);
+}
 
 extern float4 __attribute__((overloadable))
-rsMatrixMultiply(rs_matrix4x4 *m, float2 in) {
+rsMatrixMultiply(const rs_matrix4x4 *m, float2 in) {
     float4 ret;
     ret.x = (m->m[0] * in.x) + (m->m[4] * in.y) + m->m[12];
     ret.y = (m->m[1] * in.x) + (m->m[5] * in.y) + m->m[13];
@@ -129,31 +137,48 @@ rsMatrixMultiply(rs_matrix4x4 *m, float2 in) {
     ret.w = (m->m[3] * in.x) + (m->m[7] * in.y) + m->m[15];
     return ret;
 }
+extern float4 __attribute__((overloadable))
+rsMatrixMultiply(rs_matrix4x4 *m, float2 in) {
+    return rsMatrixMultiply((const rs_matrix4x4 *)m, in);
+}
 
 extern float3 __attribute__((overloadable))
-rsMatrixMultiply(rs_matrix3x3 *m, float3 in) {
+rsMatrixMultiply(const rs_matrix3x3 *m, float3 in) {
     float3 ret;
     ret.x = (m->m[0] * in.x) + (m->m[3] * in.y) + (m->m[6] * in.z);
     ret.y = (m->m[1] * in.x) + (m->m[4] * in.y) + (m->m[7] * in.z);
     ret.z = (m->m[2] * in.x) + (m->m[5] * in.y) + (m->m[8] * in.z);
     return ret;
 }
+extern float3 __attribute__((overloadable))
+rsMatrixMultiply(rs_matrix3x3 *m, float3 in) {
+    return rsMatrixMultiply((const rs_matrix3x3 *)m, in);
+}
+
 
 extern float3 __attribute__((overloadable))
-rsMatrixMultiply(rs_matrix3x3 *m, float2 in) {
+rsMatrixMultiply(const rs_matrix3x3 *m, float2 in) {
     float3 ret;
     ret.x = (m->m[0] * in.x) + (m->m[3] * in.y);
     ret.y = (m->m[1] * in.x) + (m->m[4] * in.y);
     ret.z = (m->m[2] * in.x) + (m->m[5] * in.y);
     return ret;
 }
+extern float3 __attribute__((overloadable))
+rsMatrixMultiply(rs_matrix3x3 *m, float2 in) {
+    return rsMatrixMultiply((const rs_matrix3x3 *)m, in);
+}
 
 extern float2 __attribute__((overloadable))
-rsMatrixMultiply(rs_matrix2x2 *m, float2 in) {
+rsMatrixMultiply(const rs_matrix2x2 *m, float2 in) {
     float2 ret;
     ret.x = (m->m[0] * in.x) + (m->m[2] * in.y);
     ret.y = (m->m[1] * in.x) + (m->m[3] * in.y);
     return ret;
+}
+extern float2 __attribute__((overloadable))
+rsMatrixMultiply(rs_matrix2x2 *m, float2 in) {
+    return rsMatrixMultiply((const rs_matrix2x2 *)m, in);
 }
 
 /////////////////////////////////////////////////////
