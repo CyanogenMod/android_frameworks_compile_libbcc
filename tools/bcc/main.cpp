eea4a71dcf953536a62aa502a220b864bc557f36
Zonr Chang <zonr.net@gmail.com>
2012-02-06 20:43:29 +0800

    Rewrite parseOption in command bcc.
    Such that it can read option like '-shared'.

--- tools/bcc/main.cpp
+++ tools/bcc/main.cpp
@@ -21,7 +21,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <getopt.h>
 
 #include <errno.h>
 #include <sys/stat.h>
@@ -37,18 +36,16 @@
   #if defined(__cplusplus)
     };
   #endif
-#define GETOPT_OPTIONS  "C:R"
 #else
-#define GETOPT_OPTIONS  "R"
 #endif
 
 #include <bcc/bcc.h>
 
-typedef int (*MainPtr)(int, char**);
+typedef int (*MainPtr)();
 
 // This is a separate function so it can easily be set by breakpoint in gdb.
-static int run(MainPtr mainFunc, int argc, char** argv) {
-  return mainFunc(argc, argv);
+static int run(MainPtr mainFunc) {
+  return mainFunc();
 }
 
 static void* lookupSymbol(void* pContext, const char* name) {
@@ -56,47 +53,78 @@ static void* lookupSymbol(void* pContext, const char* name) {
 }
 
 const char* inFile = NULL;
-bool printListing = false;
 bool runResults = false;
 
-extern int opterr;
-extern int optind;
+struct option_info {
+  const char *option_name;
 
-static int parseOption(int argc, char** argv)
-{
-  int c;
-  while ((c = getopt (argc, argv, GETOPT_OPTIONS)) != -1) {
-    opterr = 0;
+  // min_option_argc is the minimum number of arguments this option should
+  // have. This is for sanity check before invoking processing function.
+  unsigned min_option_argc;
 
-    switch(c) {
-    #if defined(__HOST__)
-      case 'C':
-        TARGET_TRIPLE_STRING = optarg;
-        break;
-    #endif
+  const char *argument_desc;
+  const char *help_message;
 
-      case 'R':
-        runResults = true;
-        break;
+  // The function to process this option. Return the number of arguments it
+  // consumed or < 0 if there's an error during the processing.
+  int (*process)(int argc, char **arg);
+};
 
-      case '?':
-        // ignore any error
-        break;
+// forward declaration of option processing functions
+static int do_set_tripe(int, char **);
+static int do_run(int, char **);
 
-      default:
-        // Critical error occurs
-        return 0;
-        break;
+static const struct option_info options[] = {
+#if defined(__HOST__)
+  { "C", 1, "triple", "setup the triple string.",             do_set_tripe  },
+#endif
+
+  { "R", 0, NULL,     "run root() method after successfully "
+                      "load and compile.",                    do_run        },
+};
+#define NUM_OPTIONS (sizeof(options) / sizeof(struct option_info))
+
+static int parseOption(int argc, char** argv) {
+  if (argc <= 1) {
+    return 1;
+  }
+
+  // argv[i] is the current processing arguments from command line
+  int i = 1;
+  while (i < argc) {
+    if (argv[i][0] == '-') {
+      // Find the corresponding option_info object
+      unsigned opt_idx = 0;
+      while (opt_idx < NUM_OPTIONS) {
+        if (::strcmp(&argv[i][1], options[opt_idx].option_name) == 0) {
+          const struct option_info *cur_option = &options[opt_idx];
+          if ((argc - i - 1) < 0) {
+            fprintf(stderr, "%s: '%s' requires at least %u arguments",
+                    argv[0], cur_option->option_name, cur_option->min_option_argc);
+            return 1;
+          }
+
+          int result = cur_option->process((argc - i - 1), &argv[i]);
+          if (result >= 0)
+            i += result;
+        }
+        ++opt_idx;
+      }
+      if (opt_idx >= NUM_OPTIONS) {
+        fprintf(stderr, "%s: unrecognized option '%s'", argv[0], argv[i]);
+        return 1;
+      }
+    } else {
+      if (inFile == NULL) {
+        inFile = argv[i++];
+      } else {
+        fprintf(stderr, "%s: only one input file is allowed currently.", argv[0]);
+        return 1;
+      }
     }
   }
 
-  if(optind >= argc) {
-    fprintf(stderr, "input file required\n");
-    return 0;
-  }
-
-  inFile = argv[optind];
-  return 1;
+  return 0;
 }
 
 static BCCScriptRef loadScript() {
@@ -141,7 +169,7 @@ static BCCScriptRef loadScript() {
   return script;
 }
 
-static int runMain(BCCScriptRef script, int argc, char** argv) {
+static int runMain(BCCScriptRef script) {
   MainPtr mainPointer = (MainPtr)bccGetFuncAddr(script, "main");
 
   if (!mainPointer) {
@@ -157,18 +185,14 @@ static int runMain(BCCScriptRef script, int argc, char** argv) {
 
   fprintf(stderr, "Executing compiled code:\n");
 
-  int argc1 = argc - optind;
-  char** argv1 = argv + optind;
-
-  int result = run(mainPointer, argc1, argv1);
+  int result = run(mainPointer);
   fprintf(stderr, "result: %d\n", result);
 
-  return 1;
+  return 0;
 }
 
 int main(int argc, char** argv) {
-  if(!parseOption(argc, argv)) {
-    fprintf(stderr, "failed to parse option\n");
+  if(parseOption(argc, argv)) {
     return 1;
   }
 
@@ -179,12 +203,27 @@ int main(int argc, char** argv) {
     return 2;
   }
 
-  if(runResults && !runMain(script, argc, argv)) {
+  if(runResults && runMain(script)) {
     fprintf(stderr, "failed to execute\n");
     return 6;
   }
 
   bccDisposeScript(script);
 
+  return 0;
+}
+
+/*
+ * Functions to process the command line option.
+ */
+#if defined(__HOST__)
+static int do_set_tripe(int, char **arg) {
+  TARGET_TRIPLE_STRING = arg[0];
+  return 1;
+}
+#endif
+
+static int do_run(int, char **) {
+  runResults = true;
   return 0;
 }
