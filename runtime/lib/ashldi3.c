4366a86dbe5c813ed58b482c8c52699f415df165
Bernhard Rosenkraenzer <Bernhard.Rosenkranzer@linaro.org>
2012-03-08 15:13:31 +0100

    libbcc: Don't implement __ashldi3 and __lshrdi3 with current gcc versions
    Current gcc versions (4.6, 4.7) complain about duplicate definitions
    of __ashldi3 and __lshrdi3 (defined here and in libgcc) at -O3
    
    Signed-off-by: Bernhard Rosenkraenzer <Bernhard.Rosenkranzer@linaro.org>

--- runtime/lib/ashldi3.c
+++ runtime/lib/ashldi3.c
@@ -12,6 +12,7 @@
  * ===----------------------------------------------------------------------===
  */
 
+#if !defined(__GNUC__) || (__GNUC__ < 4) || (__GNUC__ == 4 && __GNUC_MINOR__ < 6)
 #include "int_lib.h"
 
 /* Returns: a << b */
@@ -39,3 +40,4 @@ __ashldi3(di_int a, si_int b)
     }
     return result.all;
 }
+#endif
