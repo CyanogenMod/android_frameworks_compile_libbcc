07fb1ac7c2c899c3355e1fb0abbd3b4614378812
Shih-wei Liao <sliao@google.com>
2012-04-25 10:06:58 -0700

    Add CleanSpec.
    Change-Id: Ic29524d7736d53916d9f191bd9656fd3e0d44f95

--- CleanSpec.mk
+++ CleanSpec.mk
@@ -51,6 +51,7 @@ $(call add-clean-step, rm -rf $(PRODUCT_OUT)/obj/SHARED_LIBRARIES/libbcc_sha1_in
 $(call add-clean-step, rm -rf $(PRODUCT_OUT)/system/lib/libbcc_sha1.so)
 $(call add-clean-step, rm -rf $(PRODUCT_OUT)/obj/SHARED_LIBRARIES/libclcore.bc_intermediates)
 $(call add-clean-step, rm -rf $(PRODUCT_OUT)/obj/SHARED_LIBRARIES/libclcore.bc_intermediates)
+$(call add-clean-step, rm -rf $(PRODUCT_OUT)/obj/SHARED_LIBRARIES/libclcore.bc_intermediates)
 
 # ************************************************
 # NEWER CLEAN STEPS MUST BE AT THE END OF THE LIST
