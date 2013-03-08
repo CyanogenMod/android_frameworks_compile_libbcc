db8c8ae174943cf29d3fac719738881a88acc2a8
Ben Cheng <bccheng@google.com>
2013-03-08 15:01:57 -0800

    Undefine a bunch of helper function already exising in gcc 4.7.
    Once we start to link libgcc.a before libm.so, these redefined symbols will
    cause problems.
    
    DO NOT MERGE
    
    Change-Id: I564a9fbb280450bb2bea83e3e7ce3427b6192412

--- runtime/lib/udivsi3.c
+++ runtime/lib/udivsi3.c
@@ -12,7 +12,7 @@
  * ===----------------------------------------------------------------------===
  */
 
-#if !defined(__GNUC__) || __GNUC__ < 4 || (__GNUC__ == 4 && __GNUC_MINOR__ < 8) // gcc >= 4.8 implements this in libgcc
+#if !defined(__GNUC__) || __GNUC__ < 4 || (__GNUC__ == 4 && __GNUC_MINOR__ < 7) // gcc >= 4.7 implements this in libgcc
 #include "int_lib.h"
 
 /* Returns: a / b */
