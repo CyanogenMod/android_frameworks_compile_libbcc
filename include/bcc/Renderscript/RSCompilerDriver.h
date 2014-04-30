DroidSim/platform_frameworks_compile_libbcc

Improve standalone bcc for on-device compilation.
Bug: 7342767

This adds functionality for driver-writers to dynamically load plugins.
If such a plugin is loaded, we then execute any function called
rsCompilerDriverInit() from it. This function can initialize any other
state the driver-writer wants.

Change-Id: I733a6a3fc59c429a542cfcaf59a57ad231a19d01

include/bcc/Renderscript/RSCompilerDriver.h
@@ -28,7 +28,13 @@ namespace bcc {
 
 class BCCContext;
 class CompilerConfig;
+class RSCompilerDriver;
 class RSExecutable;
+
+// Type signature for dynamically loaded initialization of an RSCompilerDriver.
+typedef void (*rsCompilerDriverInit_t) (bcc::RSCompilerDriver *);
+// Name of the function that we attempt to dynamically load/execute.
+#define RSCOMPILERDRIVERINIT_FN rsCompilerDriverInit
 
 class RSCompilerDriver {
 private:
@@ -41,6 +47,9 @@ private:
 
   // Are we compiling under an RS debug context with additional checks?
   bool mDebugContext;
+
+  // Callback before linking with the runtime library.
+  RSLinkRuntimeCallback mLinkRuntimeCallback;
 
   // Do we merge global variables on ARM using LLVM's optimization pass?
   // Disabling LLVM's global merge pass allows static globals to be correctly
@@ -80,6 +89,14 @@ public:
 
   void setDebugContext(bool v) {
     mDebugContext = v;
+  }
+
+  void setLinkRuntimeCallback(RSLinkRuntimeCallback c) {
+    mLinkRuntimeCallback = c;
+  }
+
+  RSLinkRuntimeCallback getLinkRuntimeCallback() const {
+    return mLinkRuntimeCallback;
   }
 
   // This function enables/disables merging of global static variables.

tools/bcc/Main.cpp
@@ -17,6 +17,7 @@
 #include <string>
 #include <vector>
 
+#include <dlfcn.h>
 #include <stdlib.h>
 
 #include <llvm/ADT/STLExtras.h>
@@ -25,6 +26,7 @@
 #include <llvm/Support/CommandLine.h>
 #include <llvm/Support/FileSystem.h>
 #include <llvm/Support/MemoryBuffer.h>
+#include <llvm/Support/PluginLoader.h>
 #include <llvm/Support/raw_ostream.h>
 #include <llvm/Support/system_error.h>
 
@@ -46,6 +48,9 @@
 #include <bcc/Support/TargetCompilerConfigs.h>
 
 using namespace bcc;
+
+#define STR2(a) #a
+#define STR(a) STR2(a)
 
 //===----------------------------------------------------------------------===//
 // General Options
@@ -88,6 +93,10 @@ llvm::cl::alias OptTargetTripleC("C", llvm::cl::NotHidden,
                                  llvm::cl::desc("Alias for -mtriple"),
                                  llvm::cl::aliasopt(OptTargetTriple));
 #endif
+
+llvm::cl::opt<bool>
+OptRSDebugContext("rs-debug-ctx",
+    llvm::cl::desc("Enable build to work with a RenderScript debug context"));
 
 //===----------------------------------------------------------------------===//
 // Compiler Options
@@ -146,6 +155,10 @@ bool ConfigCompiler(RSCompilerDriver &pRSCD) {
 
   pRSCD.setConfig(config);
   Compiler::ErrorCode result = RSC->config(*config);
+
+  if (OptRSDebugContext) {
+    pRSCD.setDebugContext(true);
+  }
 
   if (result != Compiler::kSuccess) {
     llvm::errs() << "Failed to configure the compiler! (detail: "
@@ -206,6 +219,15 @@ int main(int argc, char **argv) {
     ALOGE("Failed to configure compiler");
     return EXIT_FAILURE;
   }
+
+  // Attempt to dynamically initialize the compiler driver if such a function
+  // is present. It is only present if passed via "-load libFOO.so".
+  rsCompilerDriverInit_t rscdi = (rsCompilerDriverInit_t)
+      dlsym(RTLD_DEFAULT, STR(RSCOMPILERDRIVERINIT_FN));
+  if (rscdi != NULL) {
+    rscdi(&RSCD);
+  }
+
   bool built = RSCD.build(context, OptOutputPath.c_str(),
       OptOutputFilename.c_str(), bitcode, bitcodeSize,
       OptBCLibFilename.c_str(), NULL, OptEmitLLVM);

CyanogenMod/android_frameworks_compile_libbcc

include/bcc/Renderscript/RSCompilerDriver.h
@@ -28,7 +28,13 @@ namespace bcc {
 
 class BCCContext;
 class CompilerConfig;
+class RSCompilerDriver;
 class RSExecutable;
+
+// Type signature for dynamically loaded initialization of an RSCompilerDriver.
+typedef void (*RSCompilerDriverInit_t) (bcc::RSCompilerDriver *);
+// Name of the function that we attempt to dynamically load/execute.
+#define RS_COMPILER_DRIVER_INIT_FN rsCompilerDriverInit
 
 class RSCompilerDriver {
 private:
@@ -41,6 +47,9 @@ private:
 
   // Are we compiling under an RS debug context with additional checks?
   bool mDebugContext;
+
+  // Callback before linking with the runtime library.
+  RSLinkRuntimeCallback mLinkRuntimeCallback;
 
   // Do we merge global variables on ARM using LLVM's optimization pass?
   // Disabling LLVM's global merge pass allows static globals to be correctly
@@ -80,6 +89,14 @@ public:
 
   void setDebugContext(bool v) {
     mDebugContext = v;
+  }
+
+  void setLinkRuntimeCallback(RSLinkRuntimeCallback c) {
+    mLinkRuntimeCallback = c;
+  }
+
+  RSLinkRuntimeCallback getLinkRuntimeCallback() const {
+    return mLinkRuntimeCallback;
   }
 
   // This function enables/disables merging of global static variables.

tools/bcc/Main.cpp
@@ -17,6 +17,7 @@
 #include <string>
 #include <vector>
 
+#include <dlfcn.h>
 #include <stdlib.h>
 
 #include <llvm/ADT/OwningPtr.h>
@@ -26,6 +27,7 @@
 #include <llvm/Support/CommandLine.h>
 #include <llvm/Support/FileSystem.h>
 #include <llvm/Support/MemoryBuffer.h>
+#include <llvm/Support/PluginLoader.h>
 #include <llvm/Support/raw_ostream.h>
 #include <llvm/Support/system_error.h>
 
@@ -47,6 +49,9 @@
 #include <bcc/Support/TargetCompilerConfigs.h>
 
 using namespace bcc;
+
+#define STR2(a) #a
+#define STR(a) STR2(a)
 
 //===----------------------------------------------------------------------===//
 // General Options
@@ -89,6 +94,10 @@ llvm::cl::alias OptTargetTripleC("C", llvm::cl::NotHidden,
                                  llvm::cl::desc("Alias for -mtriple"),
                                  llvm::cl::aliasopt(OptTargetTriple));
 #endif
+
+llvm::cl::opt<bool>
+OptRSDebugContext("rs-debug-ctx",
+    llvm::cl::desc("Enable build to work with a RenderScript debug context"));
 
 //===----------------------------------------------------------------------===//
 // Compiler Options
@@ -148,6 +157,10 @@ bool ConfigCompiler(RSCompilerDriver &pRSCD) {
   pRSCD.setConfig(config);
   Compiler::ErrorCode result = RSC->config(*config);
 
+  if (OptRSDebugContext) {
+    pRSCD.setDebugContext(true);
+  }
+
   if (result != Compiler::kSuccess) {
     llvm::errs() << "Failed to configure the compiler! (detail: "
                  << Compiler::GetErrorString(result) << ")\n";
@@ -184,6 +197,15 @@ int main(int argc, char **argv) {
     ALOGE("Failed to configure compiler");
     return EXIT_FAILURE;
   }
+
+  // Attempt to dynamically initialize the compiler driver if such a function
+  // is present. It is only present if passed via "-load libFOO.so".
+  RSCompilerDriverInit_t rscdi = (RSCompilerDriverInit_t)
+      dlsym(RTLD_DEFAULT, STR(RS_COMPILER_DRIVER_INIT_FN));
+  if (rscdi != NULL) {
+    rscdi(&RSCD);
+  }
+
   bool built = RSCD.build(context, OptOutputPath.c_str(),
       OptOutputFilename.c_str(), bitcode, bitcodeSize,
       OptBCLibFilename.c_str(), NULL, OptEmitLLVM);